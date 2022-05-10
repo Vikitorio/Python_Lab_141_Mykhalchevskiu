aa72d2b74c17cd8178e6b01a63c4a778f283abaf
Viktor <npcviktor@gmail.com>
2022-05-10 22:47:57 +0300

    feat: All

--- Laboratorna5/Laba5.5-5.py
+++ Laboratorna5/Laba5.5-5.py
@@ -1,3 +1,17 @@
 """
 5.	Описати функцію RearRange(x, y), яка перевіряє, чи можливо переставивши букви в слові х отримати слово y.
 """
+def RearRange(word1, word2):
+    word1.sort()
+    word2.sort()
+    if len(word1) != len(word2):
+        return 'Неможливо'
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            return 'Неможливо'
+    return 'Можливо'
+
+userWord = list(input("Введіть перше слово: "))
+checkWord = list(input('Введіть друге слово: '))
+print(RearRange(userWord, checkWord))
+input()
